8231ce60314b7e8daefeabf5adc0c43fc5deb16b
Minh Trịnh Xuân <trinhxuanminh2000@gmail.com>
2023-04-26 13:35:07 +0700

    Fix convert date

--- Sources/MoviePlayer/Extension/StringExtension.swift
+++ Sources/MoviePlayer/Extension/StringExtension.swift
@@ -10,7 +10,7 @@ import UIKit
 public extension String {
   func convertToDate() -> Date? {
     let dateFormatter = DateFormatter()
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    dateFormatter.dateFormat = "dd/MM/yyyy HH:mm:ss"
     return dateFormatter.date(from: self)
   }
   
